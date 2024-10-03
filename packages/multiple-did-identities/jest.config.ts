483651590eeb01adfb9735ce6746aea52cc00b03
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-10-03 14:47:13 +0100

    modified library files to use the updated storage package

--- packages/multiple-did-identities/jest.config.ts
+++ packages/multiple-did-identities/jest.config.ts
@@ -1,8 +1,14 @@
 export default {
   displayName: 'multiple-did-identities',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  moduleFileExtensions: ['ts', 'js', 'html'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(\\@adorsys-gis/storage)/)', // Do not ignore @adorsys-gis/storage
+  ],
+  extensionsToTreatAsEsm: ['.ts'], // Treat TypeScript files as ESM
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   coverageDirectory: '../../coverage/libs/multiple-did-identities',
 };
