58e7b10ed50008746dfe045882b174090d44f21b
Awambeng Rodrick <awambengrodrick@gmail.com>
2024-10-08 09:32:53 +0100

    Remove transformIgnorePatterns for event-bus from Jest configuration

--- packages/contact-service/jest.config.ts
+++ packages/contact-service/jest.config.ts
@@ -6,7 +6,6 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
-  transformIgnorePatterns: ['/node_modules/(?!(\\@adorsys-gis/event-bus)/)'],
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/contact-service',
   setupFilesAfterEnv: ['./jest.setup.ts'],
