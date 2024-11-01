dbbbde2e81570c601dc89d718af2b973bd94e180
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-11-01 16:44:14 +0100

    formated utility code base

--- packages/multiple-did-identities/src/utils/concatenateKeyStrings.ts
+++ packages/multiple-did-identities/src/utils/concatenateKeyStrings.ts
@@ -1,3 +1,3 @@
 export function concatenateKeyStrings(...strings: string[]): string {
-    return strings.join('');
+  return strings.join('');
 }
--- packages/multiple-did-identities/src/utils/convertServiceToAbbreviatedFormat.ts
+++ packages/multiple-did-identities/src/utils/convertServiceToAbbreviatedFormat.ts
@@ -1,12 +1,14 @@
 import { Service, AbbreviatedService } from '../did-methods/IDidMethod';
 
-export function convertServiceToAbbreviatedFormat(service: Service): AbbreviatedService {
-    return {
-        t: 'dm',
-        s: {
-            uri: service.serviceEndpoint.uri,
-            a: service.serviceEndpoint.accept || [],
-            r: service.serviceEndpoint.routingKeys || []
-        }
-    };
+export function convertServiceToAbbreviatedFormat(
+  service: Service,
+): AbbreviatedService {
+  return {
+    t: 'dm',
+    s: {
+      uri: service.serviceEndpoint.uri,
+      a: service.serviceEndpoint.accept || [],
+      r: service.serviceEndpoint.routingKeys || [],
+    },
+  };
 }
