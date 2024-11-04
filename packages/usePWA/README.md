2fe88e9c68efc8f21dc700e06fd07e1cefb3515b
Awambeng Rodrick <awambengrodrick@gmail.com>
2024-11-04 13:31:08 +0100

    feat(usePWA): isolate library and release to artifact repository

---
+++ packages/usePWA/README.md
@@ -0,0 +1,9 @@
+# usePWA
+
+`usepwa` is a lightweight React hook designed to simplify the integration of Progressive Web App (PWA) features into your applications. This library provides a straightforward way to manage PWA installation prompts, enabling seamless user experiences across devices. With `usePWA`, developers can easily check if their application is installable, helping to encourage users to install and engage with their web apps like native applications. Leverage this hook to enhance your app's functionality and deliver a more robust experience for your users.
+
+## Installation
+
+```bash
+npm install @adorsys-gis/usepwa
+```
---
+++ packages/usePWA/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'src/index.js',
+    library: {
+      name: 'usePWA',
+      type: 'umd',
+    },
+    globalObject: 'this',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.lib.json',
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'src/lib/components/[name].css',
+      chunkFilename: '[name].css',
+    }),
+  ],
+  externals: {
+    react: 'react',
+  },
+  stats: 'minimal',
+  mode: 'production',
+};
