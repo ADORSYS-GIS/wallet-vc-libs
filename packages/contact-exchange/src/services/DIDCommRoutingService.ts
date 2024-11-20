fff0a7e53a5043c3dfc5ea14d8e273701b06a8f7
hugoib <hugoibanez@gmail.com>
2024-11-20 16:41:39 +0100

    testing didpeer

--- packages/contact-exchange/src/services/DIDCommRoutingService.ts
+++ packages/contact-exchange/src/services/DIDCommRoutingService.ts
@@ -6,207 +6,117 @@ import {
   Secret,
 } from 'didcomm-node';
 import { v4 as uuidv4 } from 'uuid';
-import { MessageTyp, MessageType } from './DIDCommOOBInvitation';
 import axios from 'axios';
-import { validEncodedUrl } from '../tests/OOBTestFixtures';
 import { parseOOBInvitation } from './OOBParser';
+import { MessageTyp, MessageType } from './DIDCommOOBInvitation';
 
-// Mock DID Resolver
-class MediatorServiceDIDResolver implements DIDResolver {
-  knownDids: DIDDoc[];
 
-  constructor(knownDids: DIDDoc[]) {
-    this.knownDids = knownDids;
-  }
-
-  async resolve(did: string): Promise<DIDDoc | null> {
-    return this.knownDids.find((ddoc) => ddoc.id === did) || null;
-  }
-}
-
-class MediatorServiceSecretsResolver implements SecretsResolver {
-  knownSecrets: Secret[];
-
-  constructor(knownSecrets: Secret[]) {
-    this.knownSecrets = knownSecrets;
-  }
-
-  async get_secret(secretId: string): Promise<Secret | null> {
-    return this.knownSecrets.find((secret) => secret.id === secretId) || null;
-  }
-
-  async find_secrets(secretIds: string[]): Promise<string[]> {
-    return secretIds.filter((id) =>
-      this.knownSecrets.some((secret) => secret.id === id),
-    );
-  }
-}
-
-export async function processMediatorOOB(oob: string){
-  const invitation = parseOOBInvitation(oob);
-  console.log(invitation);
-  return true;
-}
-
-// This function retrieves the Out-of-Band (OOB) URL from the Mediator.
-export async function getOutOfBandInvitation() {
+// Function to process OOB invitation
+export async function processMediatorOOB(oob: string, didResolver: DIDResolver, secretsResolver: SecretsResolver) {
   try {
-    const response = await axios.get(
-      `${MessageType.RoutingAccept}/${validEncodedUrl}`,
+    // Parse and decode OOB
+    const oobUrl = oob.split('=')[1];
+    const decodedOob = JSON.parse(
+      Buffer.from(oobUrl, 'base64url').toString('utf-8'),
     );
-    const oobUrl = response.data;
-    console.log('OOB URL:', oobUrl);
-    return oobUrl;
-  } catch (error) {
-    console.error('Error fetching OOB invitation:', error);
-    throw error;
-  }
-}
+    console.log('Decoded OOB:', decodedOob);
 
-// This function sends a Mediation Request to the Mediator
-export async function requestMediation(
-  senderDid: string,
-  mediatorDid: string,
-  secretsResolver: SecretsResolver,
-  didResolver: DIDResolver,
-): Promise<string> {
-  try {
-    const response = await axios.get(
-      `${MessageType.RoutingAccept}/${validEncodedUrl}`,
-    );
-    if (!response.data) {
-      throw new Error('OOB invitation not found');
-    }
-    const oobUrl = response.data;
-    console.log('OOB URL:', oobUrl);
+    // Create Alice's DID
+    const aliceDid = ` did:peer:2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0`;
+    console.log("Alice's DID:", aliceDid);
 
-    const messageBody = {
-      type: MessageType.MediationRequest,
-      from: senderDid,
-    };
+    //const result: DIDKeyPairMethod2 = await didPeerMethod.generateMethod2();
 
-    const routedMessage = new Message({
+    // Send Mediation Request
+    const mediationRequest = new Message({
+      extra_header: [{"return_route": "all"}],
       id: uuidv4(),
       typ: MessageTyp.Didcomm,
       type: MessageType.MediationRequest,
-      from: senderDid,
-      to: [mediatorDid],
-      created_time: Math.floor(Date.now() / 1000),
-      body: messageBody,
+      body: {},
     });
 
-    const [encryptedMsg] = await routedMessage.pack_encrypted(
-      mediatorDid,
-      senderDid,
-      senderDid,
+    console.log('other did:', decodedOob.from);
+
+    const packedRequest = await mediationRequest.pack_encrypted(
+      decodedOob.from,
+      aliceDid,
+      null,
       didResolver,
       secretsResolver,
-      {
-        forward: false,
-      },
+      { forward: false },
     );
 
-    return encryptedMsg;
-  } catch (error) {
-    console.error('Error requesting mediation:', error);
-    throw error;
-  }
-}
+    console.log('packedRequest:', packedRequest);
 
-// This function sends the mediation request.
-export async function sendToMediator(encryptedMsg: string) {
-  try {
-    const response = await axios.post(`${MessageType.MediationMediate}`, {
-      message: encryptedMsg,
-    });
-    console.log('Sent to mediator:', response.data);
-  } catch (error) {
-    console.error('Error sending to mediator:', error);
-    throw error;
-  }
-}
 
-// This function handles the Mediation Response
-export async function handleMediationResponse(
-  encryptedMsg: string,
-  didResolver: DIDResolver,
-  secretsResolver: SecretsResolver,
-): Promise<void> {
-  try {
-    const [unpackedMsg] = await Message.unpack(
-      encryptedMsg,
-      didResolver,
-      secretsResolver,
-      {},
-    );
-
-    const messageBody = unpackedMsg.as_value();
-    if (messageBody.type === MessageType.MediationResponse) {
-      if (messageBody.from === messageBody.to) {
-        console.log(
-          `Received self-mediation response: ${JSON.stringify(messageBody)}`,
-        );
-        return;
-      }
-      console.log(
-        `Received mediation response: ${JSON.stringify(messageBody)}`,
-      );
+    // Send to mediator
+    const mediatorDIDDoc = await didResolver.resolve(decodedOob.from);
+    if (!mediatorDIDDoc || !mediatorDIDDoc.service || !mediatorDIDDoc.service[0].serviceEndpoint) {
+      throw new Error('Invalid mediator DID or service endpoint');
     }
+    const mediatorEndpoint = mediatorDIDDoc.service[0].serviceEndpoint;
+    console.log('Mediator Endpoint:', mediatorEndpoint);
+
+  //   const response = await axios.post(mediatorEndpoint, packedRequest.packed_msg, {
+  //     headers: { 'Content-Type': 'application/didcomm-encrypted+json' },
+  //   });
+
+  //   // Unpack Mediator Response
+  //   const mediatorResponse = await Message.unpack(
+  //     response.data,
+  //     didResolver,
+  //     secretsResolver,
+  //     {},
+  //   );
+
+  //   const mediatorRoutingKey = mediatorResponse.body.routing_did;
+  //   const mediatorNewDID = mediatorResponse.from_prior?.sub;
+  //   console.log('Mediator Routing Key:', mediatorRoutingKey);
+  //   console.log('Mediator New DID:', mediatorNewDID);
+
+  //   // Keylist Update
+  //   const aliceDIDForBob = `did:peer:${uuidv4()}`;
+  //   const keylistUpdate = new Message({
+  //     typ: '',
+  //     id: uuidv4(),
+  //     type: 'https://didcomm.org/coordinate-mediation/2.0/keylist-update',
+  //     from: aliceDid,
+  //     to: [mediatorNewDID],
+  //     body: {
+  //       updates: [
+  //         {
+  //           recipient_did: aliceDIDForBob,
+  //           action: 'add',
+  //         },
+  //       ],
+  //     },
+  //   });
+
+  //   const packedKeylistUpdate = await keylistUpdate.pack_encrypted(
+  //     mediatorNewDID,
+  //     aliceDid,
+  //     null,
+  //     didResolver,
+  //     secretsResolver,
+  //     { forward: false },
+  //   );
+
+  //   const keylistResponse = await axios.post(mediatorEndpoint, packedKeylistUpdate.packed_msg, {
+  //     headers: { 'Content-Type': 'application/didcomm-encrypted+json' },
+  //   });
+
+  //   const unpackedKeylistResponse = await Message.unpack(
+  //     keylistResponse.data,
+  //     didResolver,
+  //     secretsResolver,
+  //     {},
+  //   );
+
+  //   console.log('Keylist Update Response:', unpackedKeylistResponse);
+  //   return unpackedKeylistResponse;
   } catch (error) {
-    console.error('Error handling mediation response:', error);
+    console.error('Error processing OOB:', error);
     throw error;
   }
 }
-
-// This function handles the Keylist Update
-export async function handleKeylistUpdate(
-  senderDid: string,
-  mediatorDid: string,
-  secretsResolver: SecretsResolver,
-  didResolver: DIDResolver,
-  keyId: string,
-): Promise<string> {
-  try {
-    const messageBody = {
-      id: uuidv4(),
-      type: MessageType.KeylistUpdate,
-      from: senderDid,
-      to: [mediatorDid],
-      created_time: Math.floor(Date.now() / 1000),
-      body: {
-        type: MessageType.KeylistUpdate,
-        from: senderDid,
-        keyId: keyId,
-      },
-    };
-
-    const keyListUpdateMessage = new Message({
-      id: uuidv4(),
-      typ: MessageTyp.Didcomm,
-      type: MessageType.KeylistUpdate,
-      from: senderDid,
-      to: [mediatorDid],
-      created_time: Math.floor(Date.now() / 1000),
-      body: messageBody,
-    });
-
-    const [encryptedMsg] = await keyListUpdateMessage.pack_encrypted(
-      mediatorDid,
-      senderDid,
-      senderDid,
-      didResolver,
-      secretsResolver,
-      {
-        forward: false,
-      },
-    );
-
-    console.log(encryptedMsg);
-    return encryptedMsg;
-  } catch (error) {
-    console.error('Error handling keylist update:', error);
-    throw error;
-  }
-}
-
-export { MediatorServiceDIDResolver, MediatorServiceSecretsResolver };
