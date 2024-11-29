febd2a842f8d2f25421cade445c16846ee5d0ccc
hugoib <hugoibanez@gmail.com>
2024-11-29 12:52:21 +0100

    progress

--- packages/contact-exchange/src/services/DIDCommRoutingService.ts
+++ packages/contact-exchange/src/services/DIDCommRoutingService.ts
@@ -4,117 +4,168 @@ import {
   SecretsResolver,
   DIDDoc,
   Secret,
+  SecretType,
 } from 'didcomm-node';
 import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
 import { parseOOBInvitation } from './OOBParser';
 import { MessageTyp, MessageType } from './DIDCommOOBInvitation';
+import { DIDKeyPairMethod2 } from '@adorsys-gis/multiple-did-identities/dist/src/did-methods/IDidMethod';
+import { DidPeerMethod } from '@adorsys-gis/multiple-did-identities/dist/src/did-methods/DidPeerMethod';
+import { PeerDIDResolver } from 'did-resolver-lib';
 
+class DidcommSecretsResolver implements SecretsResolver {
+  knownSecrets: Secret[];
+
+  constructor(knownSecrets: Secret[]) {
+    this.knownSecrets = knownSecrets;
+  }
+
+  async get_secret(secretId: string): Promise<Secret | null> {
+    return this.knownSecrets.find((secret) => secret.id === secretId) || null;
+  }
+
+  async find_secrets(secretIds: string[]): Promise<string[]> {
+    return secretIds.filter((id) =>
+      this.knownSecrets.find((secret) => secret.id === id),
+    );
+  }
+}
+
+function createSecretsResolver(knownSecrets: Secret[]): DidcommSecretsResolver {
+  return new DidcommSecretsResolver(knownSecrets);
+}
 
 // Function to process OOB invitation
-export async function processMediatorOOB(oob: string, didResolver: DIDResolver, secretsResolver: SecretsResolver) {
+export async function processMediatorOOB(oob: string) {
   try {
-    // Parse and decode OOB
+    // Step 1 - Parse and decode OOB
     const oobUrl = oob.split('=')[1];
     const decodedOob = JSON.parse(
       Buffer.from(oobUrl, 'base64url').toString('utf-8'),
     );
     console.log('Decoded OOB:', decodedOob);
+    const didTo = decodedOob.from;
+    console.log('DID To:', didTo);
 
-    // Create Alice's DID
-    const aliceDid = ` did:peer:2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0`;
-    console.log("Alice's DID:", aliceDid);
+    // Step 2 - Create did
+    const didPeerMethod = new DidPeerMethod();
+    const didPeer: DIDKeyPairMethod2 = await didPeerMethod.generateMethod2();
+    console.log('result:', JSON.stringify(didPeer, null, 2));
 
-    //const result: DIDKeyPairMethod2 = await didPeerMethod.generateMethod2();
-
-    // Send Mediation Request
+    // Step 3 - Prepare Mediation Request
     const mediationRequest = new Message({
-      extra_header: [{"return_route": "all"}],
+      extra_header: [{ return_route: 'all' }],
       id: uuidv4(),
       typ: MessageTyp.Didcomm,
       type: MessageType.MediationRequest,
       body: {},
     });
 
-    console.log('other did:', decodedOob.from);
+    console.log('mediationRequest:', mediationRequest.as_value());
+
+    // Step 4 - Pack mediation request
+    const resolver = new PeerDIDResolver();
+
+    const knownSecrets: Secret[] = [];
+    didPeer.didDocument?.verificationMethod?.forEach((method) => {
+      const privateKeyJwk =
+        method.id === '#key-1'
+          ? didPeer.privateKeyV
+          : method.id === '#key-2'
+            ? didPeer.privateKeyE
+            : undefined;
+
+      if (privateKeyJwk) {
+        knownSecrets.push({
+          id: `${didPeer.did}${method.id}`,
+          type: method.type as SecretType,
+          privateKeyJwk,
+        });
+      }
+    });
+
+    // Use the knownSecrets array with the secrets resolver
+    const secretsResolver = createSecretsResolver(knownSecrets);
+
+    console.log('Secrets Resolver:', JSON.stringify(secretsResolver, null, 2));
 
     const packedRequest = await mediationRequest.pack_encrypted(
-      decodedOob.from,
-      aliceDid,
-      null,
-      didResolver,
+      didTo,
+      didPeer.did,
+      didPeer.did,
+      resolver,
       secretsResolver,
       { forward: false },
     );
 
     console.log('packedRequest:', packedRequest);
 
+    // // Send to mediator
+    // const mediatorDIDDoc = await didResolver.resolve(decodedOob.from);
+    // if (!mediatorDIDDoc || !mediatorDIDDoc.service || !mediatorDIDDoc.service[0].serviceEndpoint) {
+    //   throw new Error('Invalid mediator DID or service endpoint');
+    // }
+    // const mediatorEndpoint = mediatorDIDDoc.service[0].serviceEndpoint;
+    // console.log('Mediator Endpoint:', mediatorEndpoint);
 
-    // Send to mediator
-    const mediatorDIDDoc = await didResolver.resolve(decodedOob.from);
-    if (!mediatorDIDDoc || !mediatorDIDDoc.service || !mediatorDIDDoc.service[0].serviceEndpoint) {
-      throw new Error('Invalid mediator DID or service endpoint');
-    }
-    const mediatorEndpoint = mediatorDIDDoc.service[0].serviceEndpoint;
-    console.log('Mediator Endpoint:', mediatorEndpoint);
+    //   const response = await axios.post(mediatorEndpoint, packedRequest.packed_msg, {
+    //     headers: { 'Content-Type': 'application/didcomm-encrypted+json' },
+    //   });
 
-  //   const response = await axios.post(mediatorEndpoint, packedRequest.packed_msg, {
-  //     headers: { 'Content-Type': 'application/didcomm-encrypted+json' },
-  //   });
+    //   // Unpack Mediator Response
+    //   const mediatorResponse = await Message.unpack(
+    //     response.data,
+    //     didResolver,
+    //     secretsResolver,
+    //     {},
+    //   );
 
-  //   // Unpack Mediator Response
-  //   const mediatorResponse = await Message.unpack(
-  //     response.data,
-  //     didResolver,
-  //     secretsResolver,
-  //     {},
-  //   );
+    //   const mediatorRoutingKey = mediatorResponse.body.routing_did;
+    //   const mediatorNewDID = mediatorResponse.from_prior?.sub;
+    //   console.log('Mediator Routing Key:', mediatorRoutingKey);
+    //   console.log('Mediator New DID:', mediatorNewDID);
 
-  //   const mediatorRoutingKey = mediatorResponse.body.routing_did;
-  //   const mediatorNewDID = mediatorResponse.from_prior?.sub;
-  //   console.log('Mediator Routing Key:', mediatorRoutingKey);
-  //   console.log('Mediator New DID:', mediatorNewDID);
+    //   // Keylist Update
+    //   const aliceDIDForBob = `did:peer:${uuidv4()}`;
+    //   const keylistUpdate = new Message({
+    //     typ: '',
+    //     id: uuidv4(),
+    //     type: 'https://didcomm.org/coordinate-mediation/2.0/keylist-update',
+    //     from: aliceDid,
+    //     to: [mediatorNewDID],
+    //     body: {
+    //       updates: [
+    //         {
+    //           recipient_did: aliceDIDForBob,
+    //           action: 'add',
+    //         },
+    //       ],
+    //     },
+    //   });
 
-  //   // Keylist Update
-  //   const aliceDIDForBob = `did:peer:${uuidv4()}`;
-  //   const keylistUpdate = new Message({
-  //     typ: '',
-  //     id: uuidv4(),
-  //     type: 'https://didcomm.org/coordinate-mediation/2.0/keylist-update',
-  //     from: aliceDid,
-  //     to: [mediatorNewDID],
-  //     body: {
-  //       updates: [
-  //         {
-  //           recipient_did: aliceDIDForBob,
-  //           action: 'add',
-  //         },
-  //       ],
-  //     },
-  //   });
+    //   const packedKeylistUpdate = await keylistUpdate.pack_encrypted(
+    //     mediatorNewDID,
+    //     aliceDid,
+    //     null,
+    //     didResolver,
+    //     secretsResolver,
+    //     { forward: false },
+    //   );
 
-  //   const packedKeylistUpdate = await keylistUpdate.pack_encrypted(
-  //     mediatorNewDID,
-  //     aliceDid,
-  //     null,
-  //     didResolver,
-  //     secretsResolver,
-  //     { forward: false },
-  //   );
+    //   const keylistResponse = await axios.post(mediatorEndpoint, packedKeylistUpdate.packed_msg, {
+    //     headers: { 'Content-Type': 'application/didcomm-encrypted+json' },
+    //   });
 
-  //   const keylistResponse = await axios.post(mediatorEndpoint, packedKeylistUpdate.packed_msg, {
-  //     headers: { 'Content-Type': 'application/didcomm-encrypted+json' },
-  //   });
+    //   const unpackedKeylistResponse = await Message.unpack(
+    //     keylistResponse.data,
+    //     didResolver,
+    //     secretsResolver,
+    //     {},
+    //   );
 
-  //   const unpackedKeylistResponse = await Message.unpack(
-  //     keylistResponse.data,
-  //     didResolver,
-  //     secretsResolver,
-  //     {},
-  //   );
-
-  //   console.log('Keylist Update Response:', unpackedKeylistResponse);
-  //   return unpackedKeylistResponse;
+    //   console.log('Keylist Update Response:', unpackedKeylistResponse);
+    //   return unpackedKeylistResponse;
   } catch (error) {
     console.error('Error processing OOB:', error);
     throw error;
--- packages/contact-exchange/src/tests/DIDCommRoutingService.test.ts
+++ packages/contact-exchange/src/tests/DIDCommRoutingService.test.ts
@@ -1,24 +1,20 @@
 // import { DIDDoc, DIDResolver, Secret, SecretsResolver } from "didcomm-node";
 // import { processMediatorOOB } from "../services/DIDCommRoutingService";
 
-import { PeerDIDResolver } from 'did-resolver-lib';
+// import { PeerDIDResolver } from 'did-resolver-lib';
+import { processMediatorOOB } from '../services/DIDCommRoutingService';
 
 describe('DIDCommRoutingService', () => {
-    it('should do the mediator coordination dance from an OOB', async () => {
-        // const oob = 'https://mediator.rootsid.cloud?_oob=eyJ0eXBlIjoiaHR0cHM6Ly9kaWRjb21tLm9yZy9vdXQtb2YtYmFuZC8yLjAvaW52aXRhdGlvbiIsImlkIjoiNDM3MmIxODctMDk5Zi00MjYxLWFlZTctZjQwZWM5ZTg3Zjg3IiwiZnJvbSI6ImRpZDpwZWVyOjIuRXo2TFNtczU1NVloRnRobjFXVjhjaURCcFptODZoSzl0cDgzV29qSlVteFBHazFoWi5WejZNa21kQmpNeUI0VFM1VWJiUXc1NHN6bTh5dk1NZjFmdEdWMnNRVllBeGFlV2hFLlNleUpwWkNJNkltNWxkeTFwWkNJc0luUWlPaUprYlNJc0luTWlPaUpvZEhSd2N6b3ZMMjFsWkdsaGRHOXlMbkp2YjNSemFXUXVZMnh2ZFdRaUxDSmhJanBiSW1ScFpHTnZiVzB2ZGpJaVhYMCIsImJvZHkiOnsiZ29hbF9jb2RlIjoicmVxdWVzdC1tZWRpYXRlIiwiZ29hbCI6IlJlcXVlc3RNZWRpYXRlIiwibGFiZWwiOiJNZWRpYXRvciIsImFjY2VwdCI6WyJkaWRjb21tL3YyIl19fQ';
-        // const didResolver = new MediatorServiceDIDResolver([]);
-        // const secretsResolver = new MediatorServiceSecretsResolver([]);
+  it('should do the mediator coordination dance from an OOB', async () => {
+    const oob =
+      'https://mediator.rootsid.cloud?_oob=eyJ0eXBlIjoiaHR0cHM6Ly9kaWRjb21tLm9yZy9vdXQtb2YtYmFuZC8yLjAvaW52aXRhdGlvbiIsImlkIjoiNDM3MmIxODctMDk5Zi00MjYxLWFlZTctZjQwZWM5ZTg3Zjg3IiwiZnJvbSI6ImRpZDpwZWVyOjIuRXo2TFNtczU1NVloRnRobjFXVjhjaURCcFptODZoSzl0cDgzV29qSlVteFBHazFoWi5WejZNa21kQmpNeUI0VFM1VWJiUXc1NHN6bTh5dk1NZjFmdEdWMnNRVllBeGFlV2hFLlNleUpwWkNJNkltNWxkeTFwWkNJc0luUWlPaUprYlNJc0luTWlPaUpvZEhSd2N6b3ZMMjFsWkdsaGRHOXlMbkp2YjNSemFXUXVZMnh2ZFdRaUxDSmhJanBiSW1ScFpHTnZiVzB2ZGpJaVhYMCIsImJvZHkiOnsiZ29hbF9jb2RlIjoicmVxdWVzdC1tZWRpYXRlIiwiZ29hbCI6IlJlcXVlc3RNZWRpYXRlIiwibGFiZWwiOiJNZWRpYXRvciIsImFjY2VwdCI6WyJkaWRjb21tL3YyIl19fQ';
 
-        const aliceDid = ` did:peer:2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0`;
+    // const aliceDid = `did:peer:2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0`;
+    // const resolver = new PeerDIDResolver();
+    // const resolved = await resolver.resolve(aliceDid);
+    // console.log(resolved);
 
-        const resolver = new PeerDIDResolver();
-        const resolved = await resolver.resolve(aliceDid);
-
-        console.log(resolved);
-
-    
-        // const response = await processMediatorOOB(oob, didResolver, secretsResolver);
-        // console.log('Test Response:', response);
-      });
-    });
-
+    const response = await processMediatorOOB(oob);
+    console.log('Test Response:', response);
+  });
+});
