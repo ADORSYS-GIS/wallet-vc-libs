c39ace8d0948d63a1bf66dbb19ca8e5373a2786b
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-01-09 18:04:16 +0100

    added somes functions for key derivation using pin and also encytption and decryption usages

---
+++ packages/multiple-did-identities/src/security/security-utils.ts
@@ -0,0 +1,76 @@
+import { JWKKeys, PrivateKeyJWK } from "../did-methods/IDidMethod";
+
+export async function deriveKey(pin: string, salt: Uint8Array) {
+    const encoder = new TextEncoder();
+    const passphraseBuffer = encoder.encode(pin);
+    const saltBuffer = salt;
+
+    const key = await crypto.subtle.importKey(
+        'raw',
+        passphraseBuffer,
+        { name: 'PBKDF2' },
+        false,
+        ['deriveKey']
+    );
+
+    return crypto.subtle.deriveKey(
+        {
+            name: 'PBKDF2',
+            salt: saltBuffer,
+            iterations: 100000,
+            hash: 'SHA-256'
+        },
+        key,
+        { name: 'AES-GCM', length: 256 },
+        false,
+        ['encrypt', 'decrypt']
+    );
+}
+
+
+export async function encryptData(pin: string, secrets: JWKKeys | PrivateKeyJWK): Promise<{ salt: Uint8Array, ciphertext: ArrayBuffer, iv: Uint8Array }> {
+    const salt = crypto.getRandomValues(new Uint8Array(16));
+    const encryptionKey = await deriveKey(pin, salt);
+
+    // Encode the secrets data into an ArrayBuffer
+    const encodedData = new TextEncoder().encode(JSON.stringify(secrets));
+
+    // Generate a random IV for AES-GCM
+    const iv = crypto.getRandomValues(new Uint8Array(12));  // AES-GCM typically uses a 12-byte IV
+
+    // Encrypt the data using AES-GCM
+    const ciphertext = await crypto.subtle.encrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        encryptionKey,
+        encodedData
+    );
+
+    return { salt, ciphertext, iv };
+}
+
+export async function decryptData(
+    pin: string,
+    salt: Uint8Array,
+    iv: Uint8Array,
+    ciphertext: ArrayBuffer
+): Promise<JWKKeys | PrivateKeyJWK> {
+    // Derive the decryption key using the PIN and salt
+    const decryptionKey = await deriveKey(pin, salt);
+
+    // Decrypt the data using AES-GCM
+    const decryptedData = await crypto.subtle.decrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        decryptionKey,
+        ciphertext
+    );
+
+    // Decode the decrypted data into a JSON object
+    const decodedData = new TextDecoder().decode(decryptedData);
+    return JSON.parse(decodedData);
+}
