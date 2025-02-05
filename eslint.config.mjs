b8176320a429dcb8fd89fccafce39f3e27f4601c
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-02-05 18:13:36 +0100

    Refactor eslint config at the root of the project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,67 @@
 import eslint from '@eslint/js';
-import jsoneslint from 'eslint-plugin-jsonc';
+
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import jsoncParser from 'jsonc-eslint-parser';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  {
+    ignores: ['node_modules', '**/build/**', '**/dist/**', '**/node_modules/**'],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  jsoneslint.configs['flat/recommended-with-json'],
+  tseslint.configs.strict,
   {
-    ignores: ['!**/*', '**/node_modules/', '**/dist'],
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'simple-import-sort': pluginSimpleImportSort,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'], // Side effect imports
+            ['^react'],    // React-related imports
+            ['^@'],        // Imports starting with '@'
+            ['^[a-z]'],    // Other packages
+            ['^\\.'],      // Relative imports
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+  },
+  {
+    files: ['**/*.json'],
+    languageOptions: {
+      parser: jsoncParser,
+    },
+  },
+  {
+    files: ['**/*.d.ts', '**/*.test.ts', '**/*.test.js'],
+    rules: {
+      'no-unused-vars': 'off',
+    },
   },
 );
