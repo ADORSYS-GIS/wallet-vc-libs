4763cfcf5e594a5fad8e24df6769557780bcc342
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-02-14 09:18:03 +0100

    updated index.ts and package.json

--- packages/message-exchange/src/lib/services/index.ts
+++ packages/message-exchange/src/lib/services/index.ts
@@ -1,10 +1,11 @@
 import { CloneMethodArgs } from '@adorsys-gis/cloning-decorator';
+import { MessageRepository } from '@adorsys-gis/message-service';
 import EventEmitter from 'eventemitter3';
 import { MessageExchangeService as _MessageExchangeService } from './MessageExchangeService';
 
 // This decorator function will deep clone every argument passed to any method of the class
 // it decorates, except when it is an instance of `EventEmitter` to preserve the same event
 // bus instance between layers.
-const decorate = CloneMethodArgs({ exclude: [EventEmitter] });
+const decorate = CloneMethodArgs({ exclude: [EventEmitter, MessageRepository] });
 
 export const MessageExchangeService = decorate(_MessageExchangeService);
