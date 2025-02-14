b49b9649f5e5ba767f4aeaf26f043999f242edb8
forkimenjeckayang <forkimenjeckayang@gmail.com>
2025-02-14 09:37:24 +0100

    code formating

--- packages/message-exchange/src/lib/services/index.ts
+++ packages/message-exchange/src/lib/services/index.ts
@@ -6,6 +6,8 @@ import { MessageExchangeService as _MessageExchangeService } from './MessageExch
 // This decorator function will deep clone every argument passed to any method of the class
 // it decorates, except when it is an instance of `EventEmitter` to preserve the same event
 // bus instance between layers.
-const decorate = CloneMethodArgs({ exclude: [EventEmitter, MessageRepository] });
+const decorate = CloneMethodArgs({
+  exclude: [EventEmitter, MessageRepository],
+});
 
 export const MessageExchangeService = decorate(_MessageExchangeService);
