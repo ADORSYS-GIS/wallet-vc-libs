d49338b1d07eacc7abd4b66d1f17b6896e6a68c5
hugoib <hugoibanez@gmail.com>
2025-02-14 12:14:06 +0100

    cleanups

--- packages/message-pickup/src/tests/StatusRequest.test.ts
+++ packages/message-pickup/src/tests/StatusRequest.test.ts
@@ -33,25 +33,23 @@ describe('StatusRequest', () => {
   };
 
   it('should work!', async () => {
+
+    // (1/2) Perform the mediation coordination:
     // const oobString = 'https://mediator.socious.io?_oob=eyJpZCI6ImY2Y2M0NTZlLTU1OTYtNDI4Mi1hOGNmLTIyMmU3OWI5NWFmMCIsInR5cGUiOiJodHRwczovL2RpZGNvbW0ub3JnL291dC1vZi1iYW5kLzIuMC9pbnZpdGF0aW9uIiwiZnJvbSI6ImRpZDpwZWVyOjIuRXo2TFNrcDkyV2JRUThzQW5mSGJ5cGZVWHVUNkM3OHpWUnBOc0F6cFE3SE5rdHRpMy5WejZNa2pUTkRLbkV2Y3gyRXl0Zkw4QmVadmRHVWZFMTUzU2JlNFU3MjlNMnhkSDVILlNleUowSWpvaVpHMGlMQ0p6SWpwN0luVnlhU0k2SW1oMGRIQnpPaTh2YldWa2FXRjBiM0l1YzI5amFXOTFjeTVwYnlJc0ltRWlPbHNpWkdsa1kyOXRiUzkyTWlKZGZYMC5TZXlKMElqb2laRzBpTENKeklqcDdJblZ5YVNJNkluZHpjem92TDIxbFpHbGhkRzl5TG5OdlkybHZkWE11YVc4dmQzTWlMQ0poSWpwYkltUnBaR052YlcwdmRqSWlYWDE5IiwiYm9keSI6eyJnb2FsX2NvZGUiOiJyZXF1ZXN0LW1lZGlhdGUiLCJnb2FsIjoiUmVxdWVzdE1lZGlhdGUiLCJhY2NlcHQiOlsiZGlkY29tbS92MiJdfSwidHlwIjoiYXBwbGljYXRpb24vZGlkY29tbS1wbGFpbitqc29uIn0';
-    // console.log('Start!');
-    
     // const processEvent = waitForEvent(DidEventChannel.MediationResponseReceived);
     // await didService.processMediatorOOB(oobString);
-
     // const eventData = await processEvent;
     // console.log('Event Data:', eventData); // Log the event data
 
-    // // follow-up call
+
+    // (2/2) Perform status request (either following previous input, or load from helpers)
     // const mediatorDid = eventData.payload.from;
     // const aliceDidForMediator = eventData.payload.to[0];
-
     const mediatorDid = mediatorDidTest;
     const aliceDidForMediator = aliceDidTest;
 
     console.log('mediatorDid:', mediatorDid);
     console.log('aliceDidForMediator:', aliceDidForMediator);
-    // Pass the DidRepository instance to processStatusRequest
     await processStatusRequest(mediatorDid, aliceDidForMediator, didRepository);
   });
 
