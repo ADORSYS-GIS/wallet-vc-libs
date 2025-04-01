c2fc55a106c5ebfa4e8d6f643ebb2a882902ffea
mbunwe-victor <mbunwevicki@gmail.com>
2025-04-01 18:30:42 +0100

    fix: formating

--- packages/multiple-did-identities/jest.setup.ts
+++ packages/multiple-did-identities/jest.setup.ts
@@ -1,5 +1,6 @@
 import 'fake-indexeddb/auto';
 
+// handle console logs
 beforeAll(() => {
   jest.spyOn(console, 'log').mockImplementation(() => {});
   jest.spyOn(console, 'error').mockImplementation(() => {});
